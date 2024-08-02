bd5efb7c24ef81f3c2855e361f4bd9c0eea5db93
ktam512 <ktam512@gmail.com>
2024-08-02 13:26:25 +0700

    First connect with the chatengine.io server

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -7,7 +9,26 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
-});
+  try{
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, 
+        secret: username,
+        first_name: username},
+        {headers: {"private-key": process.env.CHAT_ENGINE_PRIVATE_KEY}}
+    );
+    return res.status(r.status).json(r.data);
+
+  } catch(error){
+    if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        return res.status(error.response.status).json(error.response.data);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error('No response received:', error.request);
+        return res.status(500).json({ error: 'No response from server' });
+  }
+}});
 
 app.listen(3001);
